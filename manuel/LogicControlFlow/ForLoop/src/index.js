b1c2af64288cb9a4427105aec84cb0c337577b36
manuel <manuel.jimenez@amaiwa.com>
2021-03-17 19:48:05 -0400

    Loop exercise added

---
+++ manuel/LogicControlFlow/ForLoop/src/index.js
@@ -0,0 +1,25 @@
+var numero = prompt("Write a number");
+while(numero % 3 = 0)
+{
+console.log("El numero es divisible entre 3");
+//without i++ Infinite Loop
+//i++;
+}
+
+
+//for(initialization;condition;final expression) 
+
+//for(let j = 0; j < 10; j++)
+//{
+//console.log(j);
+//}
+
+//Infinite Loop
+//let k = 0,
+//for( ;; )
+//{
+//console.log(j);
+//}
+
+
+
