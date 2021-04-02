085ecb1c99602b7013bfb3176bf2f1ec2fcf4306
Yueng De La Hoz <yueng.delahoz@care.ai>
2021-04-01 20:09:03 -0400

    completed instructions

--- yueng/functions/src/yueng.js
+++ yueng/functions/src/yueng.js
@@ -30,14 +30,69 @@ function temperatureConversion(op){
 	let n1 =document.getElementById("frm2").number1.value;
 	let n3=null;
 	if (op === 'F'){
-		n3 = n1*1.8 + 32
-		n3 = n3 + "°F"
+		n3 = n1*1.8 + 32;
+		let text = null;
+		if (n3 >= 10 && n3<= 32){
+			text = ", Low Temperature" ;
+		}
+		else if(n3 >= 33 && n3<= 70){
+			text = ", Nice Temperature" ;
+		}
+		else if(n3 >= 70 && n3<= 100){
+			text = ", High Temperature" ;
+		}
+		else{
+			text = ", Unknown Temperature" ;
+		}
+
+		n3 = n3 + text +  "°F";
 	}
 	else{
 		n3 = (n1-32)*(5/9)
-		n3 = n3 + "°C"
+
+		let text = null;
+		if (n1 >= 10 && n1<= 32){
+			text = ", Low Temperature" ;
+		}
+		else if(n1 >= 33 && n1<= 70){
+			text = ", Nice Temperature" ;
+		}
+		else if(n1 >= 70 && n1<= 100){
+			text = ", High Temperature" ;
+		}
+		else{
+			text = ", Unknown Temperature" ;
+		}
+
+		n3 = n3 + text +  "°C";
 	}
 
 	console.log('result',n3);
-	document.getElementById('frm2').result.value="result: " + n3;
+	document.getElementById('frm2').result.value="Result: " + n3;
+}
+
+function multiplication(){
+	let n1 = parseInt(document.getElementById("frm3").number1.value);
+	let n2 = parseInt(document.getElementById("frm3").number2.value);
+
+	op = n1 * n2;
+	let result = 0;
+	let result_str = "";
+	x = Math.abs(n1);
+	y = Math.abs(n2);
+
+	for (let i=0; i < x; i++){
+		result += y;
+		if (i == (x-1)){
+			result_str = result_str + y;
+		}
+		else{
+			result_str = result_str + y + "+";
+		}
+	}
+	if (op < 0){
+		result = -result;
+	}
+	result_str = result_str + " = " + result;
+	document.getElementById('frm3').result.value = result_str;
 }
