7b8d9efe67ec9a9d45d7a85c1bea7e0581f58c97
Juan Wilches <jwilches@usf.edu>
2021-04-14 20:08:34 -0400

    Added rally function in tennis simulation

--- juan/objects/src/tennis.js
+++ juan/objects/src/tennis.js
@@ -5,7 +5,8 @@
  * Distributed under terms of the MIT license.
  */
 
-function Racquet() {
+function Racquet(name) {
+    this.name = name;
     this.id = parseInt(Math.random()*1000);
     //Attack attributes
     this.power = Math.random()*10+1;
@@ -22,13 +23,42 @@ function Racquet() {
 	
 	ball.setDirection(direction);
 	ball.setSpeed(this.power*delta);
+	ball.hitBy = this;
 
-	if (precision*delta > 0.5) {
+	let servePrecision = precision*delta;
+
+	if (servePrecision > 0.5) {
 	    ball.setIsIn(true);
 	} else {
 	    ball.setIsIn(false);
 	}
+	console.log('Serve by:',this.name,'Weighted Reaction:',servePrecision);
 	return delta
+    }
+
+    this.hit = function(ball) {
+	    
+	let weightedReaction = (ball.getNormSpeed() + this.defense + this.getReaction())/3;
+	//weightedReaction = (1-0.4)*weightedReaction + 0.4;
+	ball.hitBy = this;
+	
+	if (weightedReaction > 0.4) {
+	    let direction = parseInt(Math.random()*2); //0: left, 1:middle, 2:right
+
+	    let delta = (1 - 0.5)*Math.random() + 0.5; 
+	    
+	    ball.setDirection(direction);
+	    ball.setSpeed(this.power*delta);
+
+	    if (precision*delta > 0.5) {
+		ball.setIsIn(true);
+	    } else {
+		ball.setIsIn(false);
+	    }
+	} else {
+		ball.setIsIn(false);
+	}
+	console.log('Hit by:',this.name,'Weighted Reaction:',weightedReaction);
     }
 
     //Defense attributes
@@ -39,13 +69,15 @@ function Racquet() {
     this.getReaction = function() {
 	return reaction;
     };
-    
+ 
 }
 
 function TennisBall() {
     let place = true;
     let direction = null;
     let speed = null;
+    const MAX_SPEED = 11;
+    this.hitBy = null;
     //Getter
     this.isIn = function() {
 	return place;
@@ -55,6 +87,9 @@ function TennisBall() {
     }
     this.getSpeed = function() {
 	return speed;
+    }
+    this.getNormSpeed = function () {
+	return speed/MAX_SPEED;
     }
 
     //Setter
@@ -72,20 +107,22 @@ function TennisBall() {
 }
 
 function startRally() {
-    let racquet1 = new Racquet();
-    console.log(racquet1);
-    let racquet2 = new Racquet();
-    console.log(racquet2);
-    let ball1 = new TennisBall();
-    console.log(ball1);
+    let r1 = new Racquet('Manuel');
+    let r2 = new Racquet('Juan');
+    let b1 = new TennisBall();
+    
+    r1.serve(b1);
+    let players = [r1,r2];
+    let idx = 0;
+    let nextPlayer = players[idx];
+    
+    let i = 0;
+    while (b1.isIn()) {
+	nextPlayer.hit(b1);
+	idx = (idx + 1) % 2;    
+	nextPlayer = players[idx];
+    }
+    console.log('Loser:',b1.hitBy.name);
 }
 
-function printChars(ball) {
-    console.log('Is in?',ball.isIn());
-    console.log('Direction?',ball.getDirection());
-    console.log('Speed?',ball.getSpeed());
-}
-
-//startRally();
-let r1 = new Racquet();
-let b1 = new TennisBall();
+startRally();
